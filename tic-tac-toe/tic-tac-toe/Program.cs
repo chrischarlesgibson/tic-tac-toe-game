953b75820ee836da4cc683e73a3bd9f884b7336e
Chris Gibson <chrischarlesgibson@gmail.com>
2023-04-18 17:32:17 -0500

    added enterXor) method and took out exttra switch case

--- tic-tac-toe/tic-tac-toe/Program.cs
+++ tic-tac-toe/tic-tac-toe/Program.cs
@@ -14,14 +14,46 @@
 
         static void Main(string[] args)
         {
+            int player = 2;//player 1 starts
+            int input = 0;
+            bool inputCorrect = true;
+
             SetField();
             Console.ReadKey();
+
+
+
+            //run code as long as program runs
+            do
+            {
+                if (player == 2)
+                {
+
+                    player = 1;
+                    EnterXorO(player, input);
+                }
+
+                else if (player == 1)
+                {
+
+                    player = 2;
+                    EnterXorO(player, input);
+                }
+
+                do
+                {
+                    Console.Write($"\nPlayer {player}: Choose your field!");
+                    input = Convert.ToInt32(Console.ReadLine());
+
+                } while (!inputCorrect);
+                
+                
+            } while (true);
         }
 
         public static void SetField()
         {
             Console.WriteLine("     |     |     ");
-            
             Console.WriteLine("  {0}  |  {1}  |  {2}", playField[0,0] , playField[0, 1],playField[0, 2]);//row0, 
             Console.WriteLine("_____|_____|_____");
             Console.WriteLine("     |     |     ");
@@ -31,5 +63,36 @@
             Console.WriteLine("  {0}  |  {1}  |  {2}", playField[2, 0], playField[2, 1], playField[2, 2]);//row 2
             Console.WriteLine("     |     |     ");
         }
+
+        public static void EnterXorO(int player, int input)
+        {
+
+
+            char playerSign = ' ';
+
+
+            if(player== 1)
+            {
+                playerSign = 'x';
+            } else if (playerSign == 2)
+            {
+                playerSign = '0';
+            }
+
+            switch (input)
+            {
+                case 1: playField[0, 0] = playerSign; break;
+                case 2: playField[0, 1] = playerSign; break;
+                case 3: playField[0, 2] = playerSign; break;
+                case 4: playField[1, 0] = playerSign; break;
+                case 5: playField[1, 1] = playerSign; break;
+                case 6: playField[1, 2] = playerSign; break;
+                case 7: playField[2, 0] = playerSign; break;
+                case 8: playField[2, 1] = playerSign; break;
+                case 9: playField[2, 2] = playerSign; break;
+            }
+        }
+                     
+        }
     }
-}
+
