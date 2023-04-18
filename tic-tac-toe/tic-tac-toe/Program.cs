01ec83f5f616d560e3e2e13b529fbed4a29b0ddb
Chris Gibson <chrischarlesgibson@gmail.com>
2023-04-18 14:49:19 -0500

    added basic tic tac toe sctrucuture

--- tic-tac-toe/tic-tac-toe/Program.cs
+++ tic-tac-toe/tic-tac-toe/Program.cs
@@ -1,9 +1,35 @@
-﻿namespace tic_tac_toe;
-class Program
+﻿namespace Tic_Tac_Toe
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello, World!");
+
+
+        //playfield
+       static char[,] playField =
+{
+    { '1', '2', '3' },//row 0
+    { '4', '5', '6' },//row1
+    { '7', '8', '9' }//row3
+};
+
+        static void Main(string[] args)
+        {
+            SetField();
+            Console.ReadKey();
+        }
+
+        public static void SetField()
+        {
+            Console.WriteLine("     |     |     ");
+            
+            Console.WriteLine("  {0}  |  {1}  |  {2}", playField[0,0] , playField[0, 1],playField[0, 2]);//row0, 
+            Console.WriteLine("_____|_____|_____");
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", playField[1, 0], playField[1, 1], playField[1, 2]);//row 1
+            Console.WriteLine("_____|_____|_____");
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", playField[2, 0], playField[2, 1], playField[2, 2]);//row 2
+            Console.WriteLine("     |     |     ");
+        }
     }
 }
-
